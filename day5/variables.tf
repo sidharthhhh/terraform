f1b99ab99151a5821e1dfed7a08f8e72ea3b0fb4
sidharth <sidharth@smarden.in>
2026-01-05 11:34:42 +0530

    day5

---
+++ day5/variables.tf
@@ -0,0 +1,13 @@
+
+# Input Variables - Values provided to Terraform configuration
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "staging"
+}
+
+variable "bucket_name" {
+  description = "S3 bucket name"
+  type        = string
+  default     = "day5_bucket"
+}
